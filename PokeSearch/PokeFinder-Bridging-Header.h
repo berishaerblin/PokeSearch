92cda9f26a77992b36ea22ef951757f1c604d86b
berishaerblin <berishaerblin@gmail.com>
2017-07-24 01:46:02 +0200

    Pokemon Finder
    Find pokemons, using firebase geolocation and apple maps

---
+++ PokeSearch/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeSearch
+//
+//  Created by Erblin Berisha on 7/23/17.
+//  Copyright © 2017 Erblin Berisha. All rights reserved.
+//
+
+#import "GeoFire.h"
